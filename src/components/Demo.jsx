e5203ab1744354d335374792473ac2e0a482e17d
Max de Vocht <maxdevocht@hotmail.com>
2024-06-19 12:46:39 +0200

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+
+import { copy, loader, linkIcon, tick } from "../assets";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* {search} */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter an URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+
+        {/* {Browse history} */}
+      </div>
+
+      {/* {Display results} */}
+    </section>
+  );
 };
 
 export default Demo;
